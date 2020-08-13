0317f495a984c0241c2b8f762910af6e04e4614b
huangjian <huangjian@huatu.com>
2020-08-13 11:40:50 +0800

    chore(config): webpack配置

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+const isProd = process.env.NODE_ENV === 'production' // 生产环境
+
+console.log(isProd)
+
+module.exports = {
+    outputDir: 'music',
+    configureWebpack: {
+        devtool: isProd ? false : 'source-map',
+        devServer: {
+            port: 8888,
+        },
+        // 外置依赖
+        externals: isProd ? {
+            vue: 'Vue',
+            'vue-router': 'VueRouter',
+            vuex: 'Vuex',
+            axios: 'axios',
+        } : {},
+    },
+    css: {
+        loaderOptions: {
+            sass: {
+                prependData: ``
+            }
+        }
+    }
+}
